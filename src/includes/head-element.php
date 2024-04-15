45dc98c461d6e0c7ac5336543406044dd2f97751
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-04-15 15:25:17 +0800

    refactor: Modularize navbar and site information to includes folder.
    You can include this on the page you're working on.

---
+++ src/includes/head-element.php
@@ -0,0 +1,12 @@
+<head>
+    <!--Site Meta Information-->
+    <meta charset="UTF-8" />
+    <title>Sweet Avenue POS</title>
+    <!--Mobile Specific Metas-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />   
+    <!--CSS-->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/main.css" />   
+    <!--Site Icon-->
+    <link rel="icon" href="images/sweet-avenue-logo.png" type="image/png"/>
+  </head>
