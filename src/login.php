a061838e1042dc3e06b8252d343aef5a9aa209b7
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-05-10 21:08:53 +0800

    feat: SPD-68-Add show password toggle button

--- src/login.php
+++ src/login.php
@@ -31,8 +31,9 @@ if(isset($_POST['sign_in_btn'])) {
     $row = $result->fetch_assoc();
 
         // Verify if password match the entered username
-        if($row['password'] == $password) {
+        if (password_verify($password, $row['password'])) {
             $_SESSION['id'] = $row['id'];
+            session_regenerate_id(true);
             header("location: index.php");
             exit();
         }
@@ -90,7 +91,35 @@ if (isset($_SESSION['error_message'])) {
     </style>
 </head>
 
-<body class="bg-rose-white ">
+<body>
+
+    <!-- Forgot Password Modal -->
+    <div class="modal fade" id="forgotPasswordModal" tabindex="-1" aria-labelledby="forgotPasswordModal" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered px-3">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="forgotPasswordModal">Forgot Password</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <form action="includes/send-password-reset.php" method="post">
+                <div class="modal-body">
+                    <div class="form-floating py-3">
+                        <input type="text" name="email" class="form-control" id="email" required>
+                        <label for="email" class="form-label text-carbon-grey fw-medium">Email Adddress<span
+                            style="color: red;"> *</span></label>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn fw-medium btn-outline-carbon-grey text-capitalize py-2 px-4 my-3"
+                        data-bs-dismiss="modal" aria-label="Close">cancel</button>
+                    <button type="submit" name="send-email-btn" class="btn fw-medium btn-medium-brown text-capitalize py-2 px-4">Send Link</button>
+                </div>
+            </form>
+            </div>
+        </div>
+      </div>
+    </div>
+
     <div class="container-fluid position-absolute top-50 start-50 translate-middle">
         <div class="row justify-content-center align-items-center">
             <div class="col-md-6 col-lg-4">
@@ -109,7 +138,7 @@ if (isset($_SESSION['error_message'])) {
                             </div>
                         <?php endif; ?>
 
-                        <div class="form-floating">
+                        <div class="form-floating mb-2">
                             <input type="text" class="form-control" onkeypress="return avoidSpace(event)" name="username" id="username" placeholder="Username" required>
                             <label for="username">Username<span style="color: red;"> *</span></label>
                             <div class="valid-feedback"></div>
@@ -118,7 +147,6 @@ if (isset($_SESSION['error_message'])) {
                             </div> 
                         </div>
                    
-                        <div class="mb-4"></div>
                         <div class="form-floating">
                             <input type="password" class="form-control" onkeypress="return avoidSpace(event)" name="password" id="password" placeholder="Password" required>
                             <label for="password">Password<span style="color: red;"> *</span></label>          
@@ -126,8 +154,16 @@ if (isset($_SESSION['error_message'])) {
                         <div class="invalid-feedback text-start">
                             Please enter a password.
                         </div>
+                        <div class="input-group mt-2 align-items-center">
+                            <div class="form-check form-switch ms-3">
+                                <input class="form-check-input fs-5" type="checkbox" id="showPassword" onclick="myFunction()">
+                                <label class="form-check-label text-carbon-grey fw-semibold pt-1" for="showPassword" style="font-size: 15px;">Show</label>
+                            </div>
+                            <a href="#" class="text-carbon-grey ms-auto nav-link fw-medium" data-bs-toggle="modal" data-bs-target="#forgotPasswordModal" style="font-size: 15px;">Forgot Password?</a>
+                        </div>
+
                         <div for="submitForm" class="justify-content-center d-md-flex pt-4">
-                            <button type="submit" name="sign_in_btn" id="submitForm" class="btn col-12 fw-bold btn-tiger-orange text-capitalize py-3 px-4">Sign In</button>
+                            <button type="submit" name="sign_in_btn" id="submitForm" class="btn col-12 fw-semibold btn-tiger-orange text-capitalize py-3 px-4">Sign In</button>
                         </div>
                     </form>
                 </div>
@@ -135,6 +171,8 @@ if (isset($_SESSION['error_message'])) {
         </div>
     </div>
     
+
+
     <script src="../vendor/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="javascript/login.js"></script>
     <script>
@@ -143,6 +181,16 @@ if (isset($_SESSION['error_message'])) {
         var k = event ? event.which : window.event.keyCode;
         if (k == 32) return false;
         }
+
+        function myFunction() {
+        var x = document.getElementById("password");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+        }
+
     </script>
 </body>
 
