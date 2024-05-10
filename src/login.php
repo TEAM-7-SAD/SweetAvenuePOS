49ee5607f3218b3aab6bc1a1018bb7e0dbca5e8b
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-05-10 15:38:56 +0800

    style: Change design

--- src/login.php
+++ src/login.php
@@ -38,14 +38,14 @@ if(isset($_POST['sign_in_btn'])) {
         }
         // If username and password mismatched, display this      
         else {
-            $_SESSION['error_message'] = 'Username and Password do not match!';
+            $_SESSION['error_message'] = 'Username and password mismatched.';
             header("Location: login.php");
             exit();
             }
     }
     // If there is no user with the username, display this
     else {
-        $_SESSION['error_message'] = 'User with this username does not exist.';
+        $_SESSION['error_message'] = 'User is not found.';
         header("Location: login.php");
         exit();
     }
@@ -85,7 +85,7 @@ if (isset($_SESSION['error_message'])) {
         .solid-color-container {
             background-color: #FFF0E9;
             padding: 40px;
-            border-radius: 50px; 
+            border-radius: 10px; 
         }
     </style>
 </head>
@@ -95,40 +95,39 @@ if (isset($_SESSION['error_message'])) {
         <div class="row justify-content-center align-items-center">
             <div class="col-md-6 col-lg-4">
                 <div class="text-center solid-color-container">
-                    <img src="images/logo-removebg-preview.png" height="200" width="200" style="margin-bottom: 20px;">
+                    <img src="images/logo-removebg-preview.png" class="mb-3" height="150" width="150">
                     <div class="text-tiger-orange text-center ">
-                        <h1 class="sweet-avenue" style="font-size: 3vw;"><dt><strong>SWEET AVENUE</strong></dt></h1>
-                        <h2 class="coffee-bakeshop mb-4" style="font-size: 2vw;"><dt><strong>COFFEE • BAKESHOP</strong></dt></h2>
+                        <h3 class="sweet-avenue fw-semibold"><strong>SWEET AVENUE</strong></h3>
+                        <h5 class="coffee-bakeshop mb-4 fw-medium"><strong>COFFEE • BAKESHOP</strong></h5>
                     </div>
                     <form action="login.php" method="post" id="login-form" class="needs-validation" novalidate>
                         
                         <?php if (isset($errorMessage)) : ?>
-                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <div class="alert border borderless bg-transparent alert-danger alert-dismissible fade show" role="alert">
                                 <strong><?php echo $errorMessage; ?></strong>
                                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                             </div>
                         <?php endif; ?>
 
                         <div class="form-floating">
-                            <input type="text" class="form-control shadow-sm" name="username" id="username" placeholder="Username" required>
+                            <input type="text" class="form-control" onkeypress="return avoidSpace(event)" name="username" id="username" placeholder="Username" required>
                             <label for="username">Username<span style="color: red;"> *</span></label>
-                            <div class="invalid-feedback"></div>
+                            <div class="valid-feedback"></div>
+                            <div class="invalid-feedback text-start">
+                                Please enter a username.
+                            </div> 
                         </div>
-                        <div class="valid-feedback"></div>
-                        <div class="invalid-feedback text-start">
-                            Please enter a Username.
-                        </div>                    
+                   
                         <div class="mb-4"></div>
-                        <div class="form-floating shadow-sm">
-                            <input type="password" class="form-control shadow-sm" name="password" id="password" placeholder="Password" required>
-                            <label for="password">Password<span style="color: red;"> *</span></label>
-                            <div class="invalid-feedback"></div>           
+                        <div class="form-floating">
+                            <input type="password" class="form-control" onkeypress="return avoidSpace(event)" name="password" id="password" placeholder="Password" required>
+                            <label for="password">Password<span style="color: red;"> *</span></label>          
                         <div class="valid-feedback"></div>
                         <div class="invalid-feedback text-start">
-                            Please enter a Password.
+                            Please enter a password.
                         </div>
                         <div for="submitForm" class="justify-content-center d-md-flex pt-4">
-                            <button type="submit" name="sign_in_btn" id="submitForm" class="btn col-12 btn-tiger-orange text-light py-3 px-3">Sign In</button>
+                            <button type="submit" name="sign_in_btn" id="submitForm" class="btn col-12 fw-bold btn-tiger-orange text-capitalize py-3 px-4">Sign In</button>
                         </div>
                     </form>
                 </div>
@@ -138,6 +137,13 @@ if (isset($_SESSION['error_message'])) {
     
     <script src="../vendor/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="javascript/login.js"></script>
+    <script>
+        // Disallow whitespaces from input fields
+        function avoidSpace(event) {
+        var k = event ? event.which : window.event.keyCode;
+        if (k == 32) return false;
+        }
+    </script>
 </body>
 
 </html>
