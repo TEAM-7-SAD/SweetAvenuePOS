6e5f82ab8750c673fe0a98637da6db29f73608dc
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-06-01 17:33:28 +0800

    fix: Remove  git module—can't be found by ci/cd

--- release.config.js
+++ release.config.js
@@ -5,14 +5,6 @@ const config = {
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
     "@semantic-release/github",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "CHANGELOG.md"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
   ],
 };
 
