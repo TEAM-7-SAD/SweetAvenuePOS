1d3d8a6b00a1c111cc2e152964e3c4eb1bd88b87
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-06-22 16:07:43 +0800

    misc

--- src/includes/db-connector.php
+++ src/includes/db-connector.php
@@ -4,12 +4,12 @@ include_once str_replace('/', DIRECTORY_SEPARATOR, 'file-utilities.php');
 include_once FileUtils::normalizeFilePath('error-reporting.php');
 
 // Creates connection to the database
-const PREFIX = 'u155023598_';
+define('PREFIX', 'u155023598_');
 
 $server = "localhost";
 $username = PREFIX . "root";
 $password = "P0SniReggie";
-$database = "sweet_avenue";
+$database = PREFIX . "sweet_avenue";
 
 $db = mysqli_connect($server, $username, $password, $database);
 
